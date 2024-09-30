c1e107e8337545a0bc4f0bf3cc24a59318d1718b
adam-3bian <adam@3bian.co.uk>
2024-09-30 13:29:18 +0100

    Minor ajustment to Debug::log output

--- src/main.cc
+++ src/main.cc
@@ -35,26 +35,30 @@ using Debug = ConditionalDebug<true, "RTCC Example">;
     i2c0->host_mode_set();
     i2c0->speed_set(100);  // Set I2C speed to 100 kHz.
 
-    // Start the oscillator
-    DS3231::read_control(i2c0, &control);
+    // Start the oscillator.
+    DS3231::read_control(i2c0, control);
     control.eosc = 0;
-    DS3231::write_control(i2c0, &control);
+    DS3231::write_control(i2c0, control);
 
     // Infinite loop: Read and display time every second.
     while (true)
     {
         // Display the current time.
-        if (DS3231::read_datetime(i2c0, &datetime))
+        if (DS3231::read_datetime(i2c0, datetime))
         {
-            Debug::log("Hours:   tens:{} units:{}", datetime.hour_tens, datetime.hour_units);
-            Debug::log("Minutes: tens:{} units:{}", datetime.minute_tens, datetime.minute_units);
-            Debug::log("Seconds: tens:{} units:{}", datetime.second_tens, datetime.second_units);
+            Debug::log("Hours:        tens:{} units:{}",
+                       datetime.hour_tens, datetime.hour_units);
+            Debug::log("Minutes:      tens:{} units:{}",
+                       datetime.minute_tens, datetime.minute_units);
+            Debug::log("Seconds:      tens:{} units:{}",
+                       datetime.second_tens, datetime.second_units);
         }
 
         // Display the current temperature.
-        if (DS3231::read_temperature(i2c0, &temperature))
+        if (DS3231::read_temperature(i2c0, temperature))
         {
-            Debug::log("Temperature: degrees:{} quarters: {}", temperature.degrees, temperature.quarters);
+            Debug::log("Temperature:  degrees:{} quarters: {}",
+                       temperature.degrees, temperature.quarters);
         }
 
         // Wait for 1 second before the next read.
